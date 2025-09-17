1c7d0cb1d2abf59b0209d5edf5fd3e6f7c99baed
Elytus <elitinho2502@hotmail.com>
2025-09-17 12:51:49 -0300

    Update

--- 1023B-Frontend/src/App.tsx
+++ 1023B-Frontend/src/App.tsx
@@ -18,16 +18,48 @@ export default function App() {
             .then(data => setProdutos(data))
     }, []);
 
+    const handleForm = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const form = event.currentTarget;
+        const formData = new FormData(form);
+
+
+        fetch('/api/produtos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => response.json())
+            .then(data => setProdutos([...produtos, data]))
+    }
+
     return (
         <div className="bg-gray-50 min-h-screen flex flex-col">
-                {produtos.map((produto) => (
-                    <div key={produto._id}>
-                        <h2>{produto.nome}</h2>
-                        <p>{produto.preco}</p>
-                        <p>{produto.descricao}</p>
-                        <p>{produto.quantidade}</p>
-                    </div>
-                ))}
+
+
+            <div>Cadastro de Produtos</div>
+            <form onSubmit={handleForm}>
+                <input type="text" name="nome" placeholder="Nome" />
+                <input type="text" name="descricao" placeholder="Descricao" />
+                <input type="number" name="preco" placeholder="Preco" />
+                <input type="number" name="quantidade" placeholder="Quantidade" />
+                <button type="submit">Cadastrar</button>
+            </form>
+
+            <h1>Produtos</h1>
+
+            {produtos.map((produto) => (
+                <div key={produto._id}>
+                    <h2>{produto.nome}</h2>
+                    <p>{produto.preco}</p>
+                    <p>{produto.descricao}</p>
+                    <p>{produto.quantidade}</p>
+                </div>
+            ))}
+
         </div>
     )
 }
