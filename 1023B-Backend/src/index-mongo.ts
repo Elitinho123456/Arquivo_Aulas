4f31b01b93f002eb3044e115c3c3296849a45058
Elytus <elitinho2502@hotmail.com>
2025-09-17 12:32:42 -0300

    Post implementado

--- 1023B-Backend/src/index-mongo.ts
+++ 1023B-Backend/src/index-mongo.ts
@@ -14,7 +14,21 @@ app.use(express.json());
 app.get('/produtos', async (req: Request, res: Response) => {
 
     const produtos = await db.collection('produtos').find().toArray();
-    res.send(produtos);
+    res.status(200).json(produtos);
+
+});
+
+app.post('/produtos', async (req: Request, res: Response) => {
+
+    const {nome, preco, descricao, quantidade} = req.body;
+    const produto = {nome, preco, descricao, quantidade};
+
+    if (!nome || !preco || !descricao || !quantidade) {
+        return res.status(400).json({message: 'Dados inválidos'});
+    }
+    
+    const result = await db.collection('produtos').insertOne(produto);
+    res.status(201).json({nome, preco, descricao, quantidade, _id:result.insertedId});
 
 });
 
